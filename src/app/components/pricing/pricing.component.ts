0b23d8b50eaf0512e959e7ddab723548da0d47fa
Manuj Rai <manuj.rai@fibre2fashion.com>
2025-02-06 12:30:24 +0530

    modified:   package-lock.json 	modified:   src/app/components/pricing/pricing.component.html 	modified:   src/app/components/pricing/pricing.component.ts

--- src/app/components/pricing/pricing.component.ts
+++ src/app/components/pricing/pricing.component.ts
@@ -1,46 +1,59 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, NgModule, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // Import FormsModule
 
 @Component({
   selector: 'app-pricing',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule ],
   templateUrl: './pricing.component.html',
   styleUrl: './pricing.component.css'
 })
 export class PricingComponent implements OnInit {
 
+  billingCycle: string = 'monthly'; // Default to monthly
+
   plans = [
     {
       name: 'Basic Plan',
-      price: 19.99,
+      monthlyPrice: 1699,
+      sixMonthsPrice: 8999,
+      yearlyPrice: 15999,
       features: [
         '1 User Access',
         'Basic Support',
         '1GB Storage',
-        'Basic Analytics'
+        'Basic Analytics',
+        'Personal Trainer (Limited Sessions)'
       ]
     },
     {
       name: 'Pro Plan',
-      price: 39.99,
+      monthlyPrice: 3399,
+      sixMonthsPrice: 17999,
+      yearlyPrice: 31999,
       features: [
         '5 User Access',
         'Priority Support',
         '5GB Storage',
-        'Advanced Analytics'
+        'Advanced Analytics',
+        'Personal Trainer (Standard Sessions)'
       ]
     },
     {
       name: 'Premium Plan',
-      price: 59.99,
+      monthlyPrice: 5099,
+      sixMonthsPrice: 26999,
+      yearlyPrice: 47999,
       features: [
         'Unlimited Users',
         '24/7 Support',
         'Unlimited Storage',
-        'Premium Analytics'
+        'Premium Analytics',
+        'Personal Trainer (Unlimited Sessions)'
       ]
     }
   ];
+  
 
   constructor() { }
 
@@ -56,4 +69,9 @@ export class PricingComponent implements OnInit {
     alert(`You selected the ${plan.name} for $${plan.price}/month.`);
   }
 
+  getPlanPrice(plan: any): number {
+    const key = `${this.billingCycle}Price` as keyof typeof plan;
+    return plan[key];
+  }
+  
 }
